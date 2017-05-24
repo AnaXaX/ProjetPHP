14dc7b4e263ba856ecb6b4bbc9bf4cbd78cadaf3
AnaXaX <anas.boudyach@gmail.com>
2017-05-24 17:57:31 +0000

    Setup
    add bootstrap
    add jquery

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Suivi pour vos étudiants</title>
+    <!--UTF-8 -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!--CSS Personnalisé -->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!--CSS bootStrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--font 
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'> </head>
+-->
+</head>
+
+<body>
+<h1>Hello World<small> Projet de Badouch & Bamhamad & Kadiri</small></h1>
+
+    <!--  jQuery  -->
+    <script src="js/jquery.min.js"></script>
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
