860663d6c4ed3b9142af9914dcf0728fe05974ec
AnaXaX <anas.boudyach@gmail.com>
2017-05-30 23:11:03 +0000

    3.3.7 bootstrap 7sen
    hadchi hsen , khedmo b mozilla rah chrome 3amr bug ma3ért 3lach

--- index.php
+++ index.php
@@ -9,29 +9,26 @@
     <!--CSS Personnalisé -->
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <!--CSS bootStrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link href="css/bootstrap.css" rel="stylesheet">
+
     <!--font 
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'> </head>
 -->
 </head>
 
 <body>
+
     <?php
-        require('navbar.php');
+        require("navbar.php");
     ?>
     
-    
-    
-    
-    
-    
-    
-    <!--  jQuery  -->
+
+
+
+
+
+
     <script src="js/jquery.min.js"></script>
-    <!--  Tether  -->
-    <script src="js/tether.min.js"></script>
-    <!-- bootstrap -->
     <script src="js/bootstrap.min.js"></script>
 </body>
 
--- navbar.php
+++ navbar.php
@@ -1,28 +1,56 @@
-<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-      <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#">AHA</a>
-      <div class="navbar-collapse collapse" id="navbarCollapse" aria-expanded="false">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp;Accueil<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
-          </li>
-        </ul>
-        <form class="form-inline mt-2 mt-md-0">
-         <a class="btn  btn-outline-secondary" href="#popup1" role="button">Login</a>
-            <div id="popup1" class="overlay">
-                <div class="popup">
-                    <h2>Here i am</h2>
-                    <a class="close" href="#">&times;</a>
-                    <div class="content">
-                        Thank to pop me out of that button, but now i'm done so you can close this window.
-                    </div>
-                </div>
-            </div>
-        </form>
-      </div>
-    </nav>
+<!-- Menu Réduit Changement -->
+<nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+    <div class="container">
+        <!-- container-fluid -->
+        <!-- bouton resized -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-badouch-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+            <!--Fin bouton resized --><a class="navbar-brand" href="#"><span class="glyphicon glyphicon-book"></span>&nbsp;&nbsp;Tamarine</a></div>
+        <div class="collapse navbar-collapse" id="bs-badouch-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Accueil</a></li>
+                <li><a href="tamarine.php"><span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;Tamarine</a></li>
+                <li><a href="contact.php"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Contact</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://www.jquery2dotnet.com">Sign Up</a></li>
+                <li class="dropdown">
+                    <a href="http://www.jquery2dotnet.com" class="dropdown-toggle" data-toggle="dropdown">Sign in <b class="caret"></b></a>
+                    <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
+                        <li>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                        <div class="form-group">
+                                            <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                        </div>
+                                        <div class="checkbox">
+                                            <label>
+                                       <input type="checkbox"> Remember me
+                                       </label>
+                                        </div>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-success btn-block">Sign in</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <input class="btn btn-primary btn-block" type="button" id="sign-in-google" value="Sign In with Google">
+                            <input class="btn btn-primary btn-block" type="button" id="sign-in-twitter" value="Sign In with Twitter">
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
